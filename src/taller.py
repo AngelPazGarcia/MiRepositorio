18365c40fc4b5c4372ef442fea30a624f002f6b0
apazgarc <apazgarc@apazgarc>
2016-01-12 11:34:46 +0100

    Hola

---
+++ src/taller.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "apazgarc"
+__date__ = "$12-ene-2016 11:32:52$"
+
+if __name__ == "__main__":
+    print "Hello World"
